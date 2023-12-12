7d7fd93dbabfba82aa2c2626598de2f86a974bc8
Cleazyy <cleazy2002@gmail.com>
2023-12-12 19:26:55 +0100

    update

--- app/index.php
+++ app/index.php
@@ -1,5 +1,21 @@
 <?php
-  header("refresh: 3"); 
+
+$servername = "db";
+$username = "fluchten";
+$password = "19";
+$dbname = "camagru";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Error: " . $conn->connect_error);
+}
+
+echo "Connected to the database successfully";
+
+$conn->close();
+
+header("refresh: 3"); 
+
 ?>
 
 <!DOCTYPE html>
