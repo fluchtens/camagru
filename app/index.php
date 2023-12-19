0d3547cdffe8b63a2062cc4cc7fa46bcc261a3d1
fluchtens <cleazy2002@gmail.com>
2023-12-19 10:27:48 +0100

    home page redirect to login if connected

--- app/index.php
+++ app/index.php
@@ -1,15 +1,19 @@
 <?php
-  session_start();
-  echo $_SESSION['id'];
+    session_start();
+    if (!isset($_SESSION['id'])) {
+        header("Location: auth/login.php");
+        exit();
+    }
+    echo $_SESSION['id'];
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-  <title>camagru</title>
+    <title>camagru</title>
 </head>
 <body>
-  <h1>Camagru</h1>
-  <h2>Hi, </h2>
+    <h1>Camagru</h1>
+    <h2>Hi, </h2>
 </body>
 </html>
