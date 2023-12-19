68a0577dbb8ab4684b0072354adbbd58490dbf9e
fluchtens <cleazy2002@gmail.com>
2023-12-19 12:12:52 +0100

    update index

--- app/index.php
+++ app/index.php
@@ -1,8 +1,20 @@
 <?php
+    require 'database.php';
+
     session_start();
     if (!isset($_SESSION['id'])) {
         header("Location: auth/login.php");
         exit();
+    } else {
+        $conn = connectToDatabase();
+        $id = $_SESSION['id'];
+        $sql = "SELECT * FROM user WHERE id = $id";
+        $result = $conn->query($sql);
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+            $username = htmlspecialchars($row['username']);
+        }
     }
 ?>
 
@@ -18,14 +30,14 @@
         <div class="container">
             <a class="main-link" href="/">camagru</a>
             <a class="login-btn" href="auth/login.php">
-                <button>Log in</button>
+                <button><?php echo $username; ?></button>
             </a>
         </div>
     </header>
     <main>
         <div class="container">
             <h1>Camagru</h1>
-            <h2>Hi, </h2>
+            <h2>Hi, <?php echo $username; ?>!</h2>
         </div>
     </main>
 </body>
