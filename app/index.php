d89806a5afdaafc263c3dc46b49c5fd231d7240d
fluchtens <cleazy2002@gmail.com>
2023-12-19 14:09:33 +0100

    update header

--- app/index.php
+++ app/index.php
@@ -2,10 +2,7 @@
     require 'database.php';
 
     session_start();
-    if (!isset($_SESSION['id'])) {
-        header("Location: auth/login.php");
-        exit();
-    } else {
+    if (isset($_SESSION['id'])) {
         $conn = connectToDatabase();
         $id = $_SESSION['id'];
         $sql = "SELECT * FROM user WHERE id = $id";
@@ -23,21 +20,14 @@
     <head>
         <meta charset="UTF-8" />
         <title>camagru</title>
-        <link rel="stylesheet" type="text/css" href="index.css">
+        <link rel="stylesheet" type="text/css" href="globals.css">
+        <link rel="stylesheet" type="text/css" href="layouts/header.css">
     </head>
 <body>
-    <header>
-        <div class="container">
-            <a class="main-link" href="/">camagru</a>
-            <a class="login-btn" href="auth/login.php">
-                <button><?php echo $username; ?></button>
-            </a>
-        </div>
-    </header>
+    <?php require 'layouts/header.php'; ?>
     <main>
         <div class="container">
-            <h1>Camagru</h1>
-            <h2>Hi, <?php echo $username; ?>!</h2>
+            <h1>Hi, <?php echo $username; ?>!</h1>
         </div>
     </main>
 </body>
---
+++ app/layouts/header.php
@@ -0,0 +1,13 @@
+<header>
+    <div class="container">
+        <a class="main-link" href="/">camagru</a>
+        <div class="links-container">
+            <a class="login-btn" href="auth/login.php">
+                <button>Log In</button>
+            </a>
+            <a class="register-btn" href="auth/register.php">
+                <button>Sign Up</button>
+            </a>
+        </div>
+    </div>
+</header>
