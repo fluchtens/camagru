db1f946f926bd56697abdfc48bad1d1129a5fb87
Cleazyy <cleazy2002@gmail.com>
2023-12-21 12:22:25 +0100

    add checks for username and password user creation

--- app/auth/register.php
+++ app/auth/register.php
@@ -8,11 +8,45 @@ if (isset($_SESSION['id'])) {
     exit();
 }
 
+function checkUsername($username) {
+    if (strlen($username) < 3 || strlen($username) > 16) {
+        return ['success' => false, 'message' => "Username must be between 3 and 16 characters long."];
+    }
+    if (!ctype_alpha($username[0])) {
+        return ['success' => false, 'message' => "Username must start with a letter."];
+    }
+    if (!preg_match('/^[a-zA-Z0-9_-]+$/', $username)) {
+        return ['success' => false, 'message' => "Username can only contain letters, digits, underscores & hyphens."];
+    }
+    return ['success' => true, 'message' => null];
+}
+
+function checkPassword($password) {
+    if (strlen($password) < 8 || strlen($password) > 30) {
+        return ['success' => false, 'message' => "Password must be between 8 and 30 characters long."];
+    }
+    if (!preg_match('/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/', $password)) {
+        return ['success' => false, 'message' => "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character."];
+    }
+    return ['success' => true, 'message' => null];
+}
+
 function registerUser($username, $password) {
     try {
         if (empty($username) || empty($password)) {
             return ['code' => 401, 'message' => "Username and password cannot be empty."];
         }
+
+        $usernameCheck = checkUsername($username);
+        if (!$usernameCheck['success']) {
+            return ['code' => 400, 'message' => $usernameCheck['message']];
+        }
+
+        $passwordCheck = checkPassword($password);
+        if (!$passwordCheck['success']) {
+            return ['code' => 400, 'message' => $passwordCheck['message']];
+        }
+
 
         $db = connectToDatabase();  
 
@@ -39,7 +73,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $response = registerUser($username, $password);
     http_response_code($response['code']);
-    if ($response['code'] == 200) {
+    if ($response['code'] === 200) {
         header("Location: login.php");
         exit();
     }
