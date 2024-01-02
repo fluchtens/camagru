8d42e3fa137f491e8e1f9926dad23168935958aa
fluchtens <cleazy2002@gmail.com>
2024-01-02 11:18:43 +0100

    fix 404 not found page

--- src/views/404.php
+++ src/views/404.php
@@ -1,6 +1,6 @@
 <div class="error-container">
     <h1>Sorry, this page isn't available.</h1>
     <p>The link you followed may be broken, or the page may have been removed.
-        <a href="/">Go back to Instagram.</a>
+        <a href="/">Go back to Camagru.</a>
     </p>
 </div>
