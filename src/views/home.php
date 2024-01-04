66c91d1b984081c43179cb4d6d4dcb438285ca64
fluchtens <cleazy2002@gmail.com>
2024-01-04 08:50:55 +0100

    fix home page

--- src/views/home.php
+++ src/views/home.php
@@ -3,28 +3,8 @@ if (!isset($_SESSION['id'])) {
     header("Location: /login");
     exit();
 }
-
-$db = connectToDatabase();
-$id = $_SESSION['id'];
-
-$query = "SELECT * FROM user WHERE id = :id";
-$stmt = $db->prepare($query);
-$stmt->bindParam(':id', $id, PDO::PARAM_INT);
-$stmt->execute();
-$user = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 
-
 <div class="main-container">
-    <?php if (isset($_SESSION['id'])): ?>
-        <!-- <div class="take-picture">
-            <video id="captureCamera" autoplay></video>
-            <button id="captureBtn">Take Photo</button>
-            <script src="scripts/capture.js"></script>
-        </div> -->
-        <?php require "./views/partials/feed.php"?>
-    <?php else: ?>
-        <p>You are not logged in.</p>
-    <?php endif; ?>
+    <?php require "./views/partials/feed.php" ?>
 </div>
-
