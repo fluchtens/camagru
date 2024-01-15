8e321da9dfb1abbf983a59e7b63a194de0d2e286
Cleazyy <cleazy2002@gmail.com>
2024-01-15 18:33:25 +0100

    starting filters

--- src/views/post.php
+++ src/views/post.php
@@ -3,12 +3,18 @@ if (!isset($_SESSION['id'])) {
     header("Location: /login");
     exit();
 }
+
+$filters = array(
+    "filter1" => "assets/filters/fire.png",
+    "filter2" => "assets/filters/pikachu.png",
+);
 ?>
 
 <div class="post">
     <h1>Create new post</h1>
     <div class="take">
         <video id="captureVideo" autoplay></video>
+        <img id="filterImg" src="assets/filters/fire.png" alt="filter">
         <button id="takePhotoBtn">Take Photo</button>
     </div>
     <div class="preview">
