c1d450f12dc599840dab65353d0374191c9a1b78
Cleazyy <cleazy2002@gmail.com>
2024-01-31 17:05:37 +0100

    create table for comments

--- src/models/post.model.php
+++ src/models/post.model.php
@@ -77,4 +77,20 @@ function unlikePost($db, $userId, $postId) {
     $stmt->bindParam(':post_id', $postId, PDO::PARAM_INT);
     $stmt->execute();
 }
+
+function getPostComments($db, $postId) {
+    $query = "SELECT
+                post_comment.*,
+                user.username as user_username, 
+                user.avatar as user_avatar
+            FROM post_comment
+            INNER JOIN user ON post_comment.user_id = user.id
+            WHERE post_id = :post_id
+    ";
+    $stmt = $db->prepare($query);
+    $stmt->bindParam(':post_id', $postId, PDO::PARAM_INT);
+    $stmt->execute();
+    $comments = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    return ($comments ? $comments : null);
+}
 ?>
--- src/views/post.php
+++ src/views/post.php
@@ -7,9 +7,26 @@ if (!isAuth()) {
 $post = getPostById($db, $uriArray[2]);
 if ($post) {
     $image = $baseUrl . "assets/uploads/posts/" . $post['file'];
+    $comments = getPostComments($db, $uriArray[2]);
 }
 ?>
 
-<div class="post">
-    <img src="<?= $image ?>" alt="<?= $post['file'] ?>">
+<div id="myModal" class="modal">
+    <div class="post">
+        <img src="<?= $image ?>" alt="<?= $post['file'] ?>">
+        <div class="comments">
+            <?php foreach($comments as $comment): ?>
+                <div class="comment">
+                    <?php $avatar = $comment['user_avatar'] ? $baseUrl . "assets/uploads/avatars/" . $comment['user_avatar'] : null; ?>
+                    <?php if ($avatar): ?>
+                        <img src="<?= $avatar; ?>" alt="<?= $comment['avatar']; ?>">
+                    <?php else: ?>
+                        <img src="<?= $baseUrl . "assets/noavatar.png"; ?>" alt="noavatar.png">
+                    <?php endif; ?>
+                    <p class="username"><?= $comment['user_username'] ?></p>
+                    <p class="message"><?= $comment['comment'] ?></p>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </div>
