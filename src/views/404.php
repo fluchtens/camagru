49da21c1d09e4b619de54929d918bab69220c3d0
Cleazyy <cleazy2002@gmail.com>
2024-02-03 15:21:07 +0100

    fix 404 error page padding

--- src/views/404.php
+++ src/views/404.php
@@ -1,4 +1,4 @@
-<div class="error-container">
+<div class="error">
     <h1>Sorry, this page isn't available.</h1>
     <p>The link you followed may be broken, or the page may have been removed.
         <a href="/">Go back to Camagru.</a>
