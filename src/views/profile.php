a3240926005f2a40db9ad53e40392ce46bb23919
Cleazyy <cleazy2002@gmail.com>
2024-02-04 10:46:27 +0100

    fix profile feed responsive

--- src/views/profile.php
+++ src/views/profile.php
@@ -32,21 +32,27 @@ $posts = getUserPosts($db, $user['id']);
         <?php if (!$posts): ?>
             <h1>No Posts Yet</h1>
         <?php else: ?>
-            <?php foreach ($posts as $post): ?>
-                <a href="<?= "/p/" . $post['id'] ?>">
-                    <img src="<?= $baseUrl . "assets/uploads/posts/" . $post['file']; ?>" alt="<?= $post['file']; ?>">
-                    <div class="stats">
-                        <div class="stat">
-                            <img class="icon" src="<?= $baseUrl . "assets/like.png"; ?>" alt="comment.png">
-                            <span class="like-count"><?= $post['like_count']; ?></span>
-                        </div>
-                        <div class="stat">
-                            <img class="icon" src="<?= $baseUrl . "assets/comment.png"; ?>" alt="comment.png">
-                            <span class="comment-count"><?= $post['comment_count']; ?></span>
-                        </div>
-                    </div>
-                </a>
-            <?php endforeach; ?>
+            <?php $count = count($posts); ?>
+            <?php for ($i = 0; $i < $count; $i += 3): ?>
+                <div class="post-group">
+                    <?php for ($j = $i; $j < min($i + 3, $count); $j++): ?>
+                        <?php $post = $posts[$j]; ?>
+                        <a href="<?= "/p/" . $post['id'] ?>">
+                            <img src="<?= $baseUrl . "assets/uploads/posts/" . $post['file']; ?>" alt="<?= $post['file']; ?>">
+                            <div class="stats">
+                                <div class="stat">
+                                    <img class="icon" src="<?= $baseUrl . "assets/like.png"; ?>" alt="comment.png">
+                                    <span class="like-count"><?= $post['like_count']; ?></span>
+                                </div>
+                                <div class="stat">
+                                    <img class="icon" src="<?= $baseUrl . "assets/comment.png"; ?>" alt="comment.png">
+                                    <span class="comment-count"><?= $post['comment_count']; ?></span>
+                                </div>
+                            </div>
+                        </a>
+                    <?php endfor; ?>
+                </div>
+            <?php endfor; ?>
         <?php endif; ?>
     </div>
 </div>
