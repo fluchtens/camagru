023b91e8f46e04fa18f2a3ae68b18a471a9fa91a
Cleazyy <cleazy2002@gmail.com>
2024-02-10 16:59:21 +0100

    fix feed

--- src/scripts/feed.js
+++ src/scripts/feed.js
@@ -68,40 +68,20 @@ function addPostToFeed(data) {
     unlikeBtn.classList.add("unlikeBtn");
     unlikeBtn.setAttribute("data-post-id", data.id);
 
-    const unlikeSvg = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "svg"
-    );
-    unlikeSvg.setAttribute("aria-label", "Unlike");
-    unlikeSvg.setAttribute("fill", "currentColor");
-    unlikeSvg.setAttribute("width", "24");
-    unlikeSvg.setAttribute("height", "24");
-    unlikeSvg.setAttribute("role", "img");
-    unlikeSvg.setAttribute("viewBox", "0 0 48 48");
-    unlikeSvg.innerHTML =
-      '<title>Unlike</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path>';
+    const unlikeImg = document.createElement("img");
+    unlikeImg.src = baseUrl + "assets/unlikeBtn.png";
 
-    unlikeBtn.appendChild(unlikeSvg);
+    unlikeBtn.appendChild(unlikeImg);
     buttons.appendChild(unlikeBtn);
   } else {
     const likeBtn = document.createElement("button");
     likeBtn.classList.add("likeBtn");
     likeBtn.setAttribute("data-post-id", data.id);
 
-    const likeSvg = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "svg"
-    );
-    likeSvg.setAttribute("aria-label", "Like");
-    likeSvg.setAttribute("fill", "currentColor");
-    likeSvg.setAttribute("width", "24");
-    likeSvg.setAttribute("height", "24");
-    likeSvg.setAttribute("role", "img");
-    likeSvg.setAttribute("viewBox", "0 0 24 24");
-    likeSvg.innerHTML =
-      '<title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path>';
+    const likeImg = document.createElement("img");
+    likeImg.src = baseUrl + "assets/likeBtn.png";
 
-    likeBtn.appendChild(likeSvg);
+    likeBtn.appendChild(likeImg);
     buttons.appendChild(likeBtn);
   }
 
@@ -109,20 +89,10 @@ function addPostToFeed(data) {
   commentLink.href = `/c/${data.id}`;
   commentLink.classList.add("comment");
 
-  const commentSvg = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "svg"
-  );
-  commentSvg.setAttribute("aria-label", "Comment");
-  commentSvg.classList.add("x1lliihq", "x1n2onr6", "x5n08af");
-  commentSvg.setAttribute("fill", "currentColor");
-  commentSvg.setAttribute("height", "24");
-  commentSvg.setAttribute("role", "img");
-  commentSvg.setAttribute("viewBox", "0 0 24 24");
-  commentSvg.innerHTML =
-    '<title>Comment</title><path d="M20.656 17.008a9.993 9.993 0 1 0-3.59 3.615L22 22Z" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="2"></path>';
+  const commentImg = document.createElement("img");
+  commentImg.src = baseUrl + "assets/commentBtn.png";
 
-  commentLink.appendChild(commentSvg);
+  commentLink.appendChild(commentImg);
   buttons.appendChild(commentLink);
 
   actions.appendChild(buttons);
@@ -151,16 +121,40 @@ function addPostToFeed(data) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const loading = document.getElementById("loadingIcon");
+  const home = document.getElementById("home");
+  let loading = undefined;
   let page = 1;
-  let lastPage = false;
+  let firstLoad = true;
   let loadingInProgress = false;
+  let lastPage = false;
 
   async function loadMorePosts() {
     const data = await getPosts(page);
-    if (!data || !data.length) {
+
+    if (firstLoad) {
+      if (!data) {
+        const title = document.createElement("h1");
+        title.textContent = "No Posts Yet";
+        home.appendChild(title);
+      } else {
+        const feed = document.createElement("ul");
+        feed.id = "feed";
+        home.appendChild(feed);
+
+        const loadingImg = document.createElement("img");
+        loadingImg.id = "loadingIcon";
+        loadingImg.classList.add("loading");
+        loadingImg.src = baseUrl + "assets/loading.gif";
+        home.appendChild(loadingImg);
+        loading = document.getElementById("loadingIcon");
+      }
+      firstLoad = false;
+    }
+
+    if (!data) {
       lastPage = true;
     }
+
     if (data && data.length > 0) {
       for (const post of data) {
         addPostToFeed(post);
@@ -173,7 +167,7 @@ document.addEventListener("DOMContentLoaded", () => {
       window.innerHeight + window.scrollY >=
       document.body.offsetHeight - 100
     ) {
-      if (!lastPage && !loadingInProgress) {
+      if (!loadingInProgress && !lastPage) {
         loading.style.display = "block";
         loadingInProgress = true;
         page++;
--- src/scripts/like.js
+++ src/scripts/like.js
@@ -20,49 +20,42 @@ async function likePost(postId) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  const feed = document.getElementById("feed");
+  const home = document.getElementById("home");
 
-  feed.addEventListener("click", async (e) => {
-    const target = e.target;
+  if (home) {
+    home.addEventListener("click", async (e) => {
+      const target = e.target;
 
-    const likeBtn = target.closest(".likeBtn");
-    const unlikeBtn = target.closest(".unlikeBtn");
+      const likeBtn = target.closest(".likeBtn");
+      const unlikeBtn = target.closest(".unlikeBtn");
 
-    if (likeBtn || unlikeBtn) {
-      const btn = likeBtn || unlikeBtn;
-      const postId = btn.getAttribute("data-post-id");
+      if (likeBtn || unlikeBtn) {
+        const btn = likeBtn || unlikeBtn;
+        const postId = btn.getAttribute("data-post-id");
 
-      const req = await likePost(postId);
-      if (!req.success) {
-        if (req.code === 401) {
-          window.location.href = "/login";
-        }
-      } else {
-        const svg = btn.querySelector("svg");
-        const path = btn.querySelector("path");
-        const likeCount = btn.closest(".actions").querySelector(".likeCount");
-        const currentLikes = parseInt(likeCount.textContent);
-
-        if (likeBtn) {
-          svg.setAttribute("viewBox", "0 0 48 48");
-          path.setAttribute(
-            "d",
-            "M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"
-          );
-          likeBtn.classList.remove("likeBtn");
-          likeBtn.classList.add("unlikeBtn");
-          likeCount.textContent = currentLikes + 1 + " likes";
+        const req = await likePost(postId);
+        if (!req.success) {
+          if (req.code === 401) {
+            window.location.href = "/login";
+          }
         } else {
-          svg.setAttribute("viewBox", "0 0 24 24");
-          path.setAttribute(
-            "d",
-            "M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"
-          );
-          unlikeBtn.classList.remove("unlikeBtn");
-          unlikeBtn.classList.add("likeBtn");
-          likeCount.textContent = currentLikes - 1 + " likes";
+          const img = btn.querySelector("img");
+          const likeCount = btn.closest(".actions").querySelector(".likeCount");
+          const currentLikes = parseInt(likeCount.textContent);
+
+          if (likeBtn) {
+            img.src = baseUrl + "assets/unlikeBtn.png";
+            likeBtn.classList.remove("likeBtn");
+            likeBtn.classList.add("unlikeBtn");
+            likeCount.textContent = currentLikes + 1 + " likes";
+          } else {
+            img.src = baseUrl + "assets/likeBtn.png";
+            unlikeBtn.classList.remove("unlikeBtn");
+            unlikeBtn.classList.add("likeBtn");
+            likeCount.textContent = currentLikes - 1 + " likes";
+          }
         }
       }
-    }
-  });
+    });
+  }
 });
